d47973090a7cf4a5851b6eadcfa4840459489045
KingK619 <53334790+KingK619@users.noreply.github.com>
2020-03-20 19:34:50 +0530

    Update operator_overloading.cpp

--- operator_overloading.cpp
+++ operator_overloading.cpp
@@ -38,12 +38,29 @@ public:
         P.y=y/rhs.y;
         return P;
     }
+    
+    Point operator++()              // overloading unary operators (post-increment)
+    {
+        ++x; ++y;
+        return Point(x,y);
+    }
+    Point operator--()              // overloading post-decrement operator
+    {
+        (x)--; (y)--;
+        return Point(x,y);
+    }
 
     void Display() { cout<<"X : "<<x<<setw(5)<<"Y : "<<y<<endl;}
 };
 
 int main()
-{
+{   
+    Point h1(5,6),h2(1,1);
+    ++h1;               
+    --h2;            
+    h2.Display();
+    h1.Display();
+    
     Point p1(10,20),p2(3,4);
     Point p3=p1+p2;
     Point p4=p1-p2;
